5915318d09d42bff3f78bf2b1d8fac4696938be2
Antonin Pasquier <antonin.pasquier45@gmail.com>
2024-01-22 22:13:41 +0100

    ajout de la video de HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import video from "../public/aolockvid.mp4";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <div className="video-container">
+        <video className="video" preload="none" autoPlay playsInline muted loop>
+          <source src={video} type="video/mp4" />
+          <source src={video} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
