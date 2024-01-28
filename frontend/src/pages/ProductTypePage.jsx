e9be09d8c159e63e1d52caa06f1a19f9c7bdc435
Antonin Pasquier <antonin.pasquier45@gmail.com>
2024-01-28 11:23:47 +0100

    rajout et importation du composent pour filter

--- frontend/src/pages/ProductTypePage.jsx
+++ frontend/src/pages/ProductTypePage.jsx
@@ -1,46 +1,14 @@
-// ProductTypePage.jsx
-import React, { useState } from "react";
+import React from "react";
 import { useLoaderData } from "react-router-dom";
-import Allproducts from "../components/AllProductCard";
+import ProductTypeFilter from "../components/ProductTypeFilter";
 
 function ProductTypePage() {
-  const { products, typeLabel } = useLoaderData();
-  const [order, setOrder] = useState("pertinence");
+  const { typeLabel } = useLoaderData();
 
-  const changeOrder = (orders) => {
-    setOrder(orders);
-  };
-
-  function sortProducts(product, sortOrder) {
-    const orderProducts = [...product];
-
-    orderProducts.sort((a, b) => {
-      switch (sortOrder) {
-        case "croissant":
-          return a.price - b.price;
-        case "decroissant":
-          return b.price - a.price;
-        case "pertinence":
-          return a.id - b.id;
-        default:
-          return 0;
-      }
-    });
-
-    return orderProducts;
-  }
   return (
     <div>
       <h1>{typeLabel}</h1>
-      <div>
-        <div>trier par :</div>
-        <select value={order} onChange={(e) => changeOrder(e.target.value)}>
-          <option value="croissant">Tri croissant</option>
-          <option value="decroissant">Tri décroissant</option>
-          <option value="pertinence">Pertinence</option>
-        </select>
-      </div>
-      <Allproducts products={sortProducts(products, order)} />
+      <ProductTypeFilter />
     </div>
   );
 }
